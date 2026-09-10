10aa449de04a23df37f515e756b4d3c9fbf9fc91
Samat <s.murzagaleev@mininform74.ru>
2026-09-10 17:30:28 +0500

    Update

--- golang/file_reader/main.go
+++ golang/file_reader/main.go
@@ -1,30 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 const start = `
 _______________
 File reader
 _______________
 Введите цифру для продолжения:
-
+0. Exit
 1. Чтение файла и вывод данных в консоль
 2. Запись строки в файл
 `
 
 const read_file = `
 Выберите способо чтения:
+0. <Назад
 1. Построчно(пакет bufioReader)
 2. Целиком(пакет io)
 `
 
 func main() {
-	var command int
+	var command, level int
+	for {
+		fmt.Println(start)
+		fmt.Scanln(&level)
+		switch level {
+		case 0:
+			return
+		case 1:
+			fmt.Println(read_file)
+			fmt.Scanln(&command)
+			switch command {
+			case 0:
+				continue
+			case 1:
+				read_string()
+				fmt.Scanln(&command)
+			}
+		}
+	}
+}
 
-	fmt.Println(start)
-	fmt.Scanln(&command)
+func read_string() {
+	var path string
+	fmt.Println("Введите путь к файлу для вывода содержимого построчно(txt):")
+	fmt.Scanln(&path)
+	file, err := os.Open(path)
 
-	if command == 1 {
-		fmt.Println(read_file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
 	}
 }
